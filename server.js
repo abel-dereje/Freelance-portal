1e1730ac4dfda0c55c6a0fdb7c6fe145a1ac0d9e
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2023-12-03 11:37:33 +0300

    creating and installing express and server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+// creating express application 
+const app= express();
+
+// port number 
+const port=4000;
+
+// i have created the express application then the express app should be listening on port
+app.listen(port,()=>{
+    console.log()
+});
