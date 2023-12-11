20e7c79cbad997c7eab7c44dab7269e493f7e136
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-12 00:01:39 +0300

    Adding nodemons dependencies on the project

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express=require('express');
 // creating express application 
 const app= express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({message:"This first server is created by express"})
+})
+
+app.post('/', (req, res) => {
+    app.status(200).json({message:"This first server is created by express"})
+})
+
 // port number 
 const port=4000;
 
