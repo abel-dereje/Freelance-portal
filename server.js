54afffe16e686fbf3b7953b1a5820f69fca06552
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-14 23:43:01 +0300

    Creating route on the server page

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const router= express.Router();
 // port number 
 const port=4000;
 
+// Route to Login Page
+app.get('/login', (req, res) => {
+    res.send("this is a login page")
+});
+
+// Route to Login Page
+app.get('/login', (req, res) => {
+    res.send("this is a signup page")
+});
+
 // i have created the express application then the express app should be listening on port
 app.listen(port,()=>{
     console.log(`Server listing or running on port ${port}`);
