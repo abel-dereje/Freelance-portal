f4056e65248fe7b259daf378a802ff13d7ad589d
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-14 23:41:35 +0300

    Creating login route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,14 +3,17 @@ const router = express.Router()
 
 
 router.post('/login', (req, res) => {
-    res.status(200).json({message:"This first server is created by express"})
+    // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
 })
 
-router.post('/signup', (req, res) => {
-    app.status(200).json({message:"This first server is created by express"})
-})
-router.post('/logout', (req, res) => {
-    app.status(200).json({message:"This first server is created by express"})
-})
+// router.post('/signup', (req, res) => {
+//     app.status(200).json({message:"This first server is created by express"})
+// })
+// router.post('/logout', (req, res) => {
+//     app.status(200).json({message:"This first server is created by express"})
+// })
 
 module.exports = router;
