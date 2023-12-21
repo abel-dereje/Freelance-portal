9437ad8b1f5d4772993e476c8fc65c09378d8cd6
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-21 20:46:19 +0300

    Creating signup route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,23 +1,25 @@
 const express = require('express')
-const { signup } = require('../controllers/user.controller')
+const  signup  = require('../controllers/user.controller')
 
-const route = express.Router()
+const router = express.Router()
 
-// Routs for signup
-route.post('/signup', signup);
+// Routs for signup, login and logout
+// route.route('/signup', signup);
+router.route('/signup')
+  .post(signup);
 
 // Routs for login
-route.post('/login', (req, res) => {
+// route.post('/login', (req, res) => {
     
-  let username = req.body.username;
-  let password = req.body.password;
-  res.send(`Username: ${username} Password: ${password}`);
-})
+//   let username = req.body.username;
+//   let password = req.body.password;
+//   res.send(`Username: ${username} Password: ${password}`);
+// })
 
 // Routs for logout
-route.post('/logout', (req, res) => {
-  app.status(200).json({message:"This first server is created by express"})
-})
+// route.post('/logout', (req, res) => {
+//   app.status(200).json({message:"This first server is created by express"})
+// })
 
 
-module.exports = route;
+module.exports = router;
