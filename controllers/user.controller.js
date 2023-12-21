7535ef4e1a677b1ffaba5e557bcdfa042566fd47
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-22 01:02:34 +0300

    Creating the logic of logout on the user controller page

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,32 +20,31 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-      // Check if email and password exist in the request body
-      if (!email || !password) {
-        throw new Error("Invalid email or password");
-      }
-  
-      // Assuming loginUser represents the retrieved user data after validation
-      const foundUser = await userModel.findOne({ email });
-  
-      if (!foundUser || foundUser.password !== password) {
-        throw new Error("Invalid email or password");
-      }
-  
-      res.status(200).json({ message: "User logged in", user: foundUser });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  const { email, password } = req.body;
+  try {
+    // Check if email and password exist in the request body
+    if (!email || !password) {
+      throw new Error("Invalid email or password");
     }
-  };
 
-  const logout = (req, res) => {
-  
-    // You can clear the token stored on the client-side upon logout
-    res.clearCookie('authToken'); // Clear the JWT token from cookies
-    
-    res.status(200).json({ message: 'User logged out successfully' });
-  };
-  
-  module.exports = { signup, login,logout };
+    // Assuming loginUser represents the retrieved user data after validation
+    const foundUser = await userModel.findOne({ email });
+
+    if (!foundUser || foundUser.password !== password) {
+      throw new Error("Invalid email or password");
+    }
+
+    res.status(200).json({ message: "User logged in", user: foundUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const logout = (req, res) => {
+  // You can clear the token stored on the client-side upon logout
+  res.clearCookie("authToken"); // Clear the JWT token from cookies
+
+  res.status(200).json({ message: "User logged out successfully" });
+};
+
+module.exports = { signup, login, logout };
