7fc96de1c830d3e3855a6aaacf8ec363b033da60
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2023-12-21 17:16:51 +0300

    Sampling

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+// importing package using require 
 const express=require('express');
+
+// apply your Database on the top level
+
 
 // creating express application 
 const app= express();
+
+// MiddleWare JSON  File for Client to request
+
+
+// Middleware As Application level of Routers 
+
+
 
 // port number 
 const port=4000;
