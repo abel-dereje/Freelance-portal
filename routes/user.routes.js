f56bf95a4f4fe25694ad0e48cfcf87e84f193192
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-22 00:45:45 +0300

    Creating login route on routing page

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,12 @@
-const express = require('express')
-const  signup  = require('../controllers/user.controller')
+const express = require("express");
+const { signup, login } = require("../controllers/user.controller");
 
-const router = express.Router()
+const router = express.Router();
 
-// Routs for signup, login and logout
-// route.route('/signup', signup);
-router.route('/signup')
-  .post(signup);
+// Routes for signup
+router.route("/signup").post(signup);
 
-// Routs for login
-// route.post('/login', (req, res) => {
-    
-//   let username = req.body.username;
-//   let password = req.body.password;
-//   res.send(`Username: ${username} Password: ${password}`);
-// })
-
-// Routs for logout
-// route.post('/logout', (req, res) => {
-//   app.status(200).json({message:"This first server is created by express"})
-// })
-
+// Routes for login
+router.route("/login").post(login);
 
 module.exports = router;
