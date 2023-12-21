f06374de95326d9cd1b9514b8474924944f5d6c7
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-21 20:45:06 +0300

    Creating signup function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,17 +5,19 @@ const userModel = require('../models/user.model')
 const signup= async(req, res) => {
 
     //Destruct the data
-    const {userName, email, password} = req.body;
+    const { userName,email,password } = req.body;
 
     //Validate the fields whether the user filled all the required fields
-    if (!userName || !email || !password) {
-        res.status(404)
-        throw new Error('all files must be filled')
+    try {
+        if (!userName || !email || !password) {
+            throw new Error('All fields must be filled');
+        }
+    
+        const createUser = await userModel.create({ userName, email, password });
+        res.status(200).json({ message: "User created", user: createUser });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    else {
-        const createUser = await userModel.create({userName,email,password});
-    }
+}
 
-    res.status(200).json("Create User", createUser)
-  }
   module.exports = signup;
