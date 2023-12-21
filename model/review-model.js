27661eacc28c272b4fb43859c38f647b5c8c1c42
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2023-12-21 16:44:36 +0300

    star and description

--- model/review-model.js
+++ model/review-model.js
@@ -10,7 +10,16 @@ const skillSchema = mongoose.Schema({
     required:true
     
    },
-  
+  star:{
+    type:Number,
+    required:true,
+    enum:[1,2,3,4,5]
+  },
+  desc:{
+    type:String,
+    required:true,
+
+  }
 
 },
 {
