65b525056b5b1c77e50914f76de4c6c32dafec6f
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2023-12-21 16:56:43 +0300

    Message Schema Artribute

--- model/message-model.js
+++ model/message-model.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
-   
+    conversationId: {
+        type: String,
+        required: true,
+      },
+      userId: {
+        type: String,
+        required: true,
+      },
+      desc: {
+        type: String,
+        required: true,
+      },
 },
 {
   timestamp:true,
