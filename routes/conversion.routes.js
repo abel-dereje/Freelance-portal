4b8c3c108ab0d5b008c910d98f2da2b17316fbeb
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-23 00:37:11 +0300

    Creating the routes for the conversion

--- routes/conversion.routes.js
+++ routes/conversion.routes.js
@@ -1,26 +1,26 @@
 const express = require('express')
-const conversion = require('../controllers/conversion.controller');
+const { conversions, getAllConversions } = require('../controllers/conversion.controller');
 
-const router = express.router();
+const routerConversion = express.Router();
 
 // GET all conversions
-router.route('/conversions')
-.get(conversions)
+routerConversion.route('/conversions')
+.get(getAllConversions )
 
 // POST a new conversion
-router.route('/conversions')
+routerConversion.route('/conversions')
 .post(conversions)
 
-// GET a specific conversion by ID
-router.route('/conversions/:id')
-.get(conversions.id)
+// // GET a specific conversion by ID
+// routerConversion.route('/conversions/:id')
+// .get(conversions.id)
 
-// PUT (update) a specific conversion by ID
-router.route('/conversions/:id')
-.put(conversions.id)
+// // PUT (update) a specific conversion by ID
+// routerConversion.route('/conversions/:id')
+// .put(conversions.id)
 
-// DELETE a specific conversion by ID
-router.route('/conversions/:id')
-.delete(conversions.id)
+// // DELETE a specific conversion by ID
+// routerConversion.route('/conversions/:id')
+// .delete(conversions.id)
 
-module.exports = router;
+module.exports = routerConversion;
