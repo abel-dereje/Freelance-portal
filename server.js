af8e0ddbf9c99296a65a276ec001b3e8cee8db85
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-23 00:35:20 +0300

    Adding middleware and path on the entry point page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router = require('./routes/user.routes');
+const routerConversion = require('./routes/conversion.routes');
 const db_connect = require('./config/db')
 
 // Calling database function
@@ -12,6 +13,7 @@ const app = express();
 // Middleware JSON file for client requests
 app.use(express.json());
 app.use(router);
+app.use(routerConversion);
 
 // Error handlers Middleware
 
