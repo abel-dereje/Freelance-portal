34a5c125f16d082a1ad3fa68c2498eb214a93369
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-22 22:26:59 +0300

    Creating the conversion model of the project

---
+++ models/conversion.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const conversionSchema = mongoose.Schema({
+
+    id:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    sellerId:{
+        type: String,
+        required: true
+    },
+    buyerId:{
+        type: String,
+        required: true
+    },
+    readBySeller: {
+        type: Boolean,
+        required: true,
+    },
+      readByBuyer: {
+        type: Boolean,
+        required: true,
+    },
+      lastMessage: {
+        type: String,
+        required: false,
+    },
+    
+    },
+    {
+      timestamp:true,
+    }
+     
+    );
+    
+    module.exports = mongoose.model("Conversion", conversionSchema);
