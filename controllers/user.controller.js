6f9fd3e770662751b7fea714c054dccd04ceedc9
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-31 01:44:53 +0300

    Creating all logics of sign up user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,60 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 require("../routes/user.routes");
 const userModel = require("../models/user.model");
 
-const signup = async (req, res) => {
+const signupUsers = async (req, res) => {
   //Destruct the data
-  const { userName, email, password } = req.body;
+  const { fullName, email, password, image, phone, address, location, bio, isSeller } = req.body;
 
   //Validate the fields whether the user filled all the required fields
-  try {
-    if (!userName || !email || !password) {
-      throw new Error("All fields must be filled");
+    if ( !email || !password) {
+      throw new Error("Email and password must be filled");
     }
 
-    const createUser = await userModel.create({ userName, email, password });
-    res.status(200).json({ message: "User created", user: createUser });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+    const user_Email = await userModel.findOne({email});
+    if(user_Email) {
+        res.status(400);
+        throw new Error ("User email is already Exists");
+    }
+    else{
+      const passwordHash = await bcrypt.hash(password,13);
+      // console.log("The Hash Password is: " + passwordHash);
+
+      const createUser = await userModel.create({
+        fullName,
+        email,
+        password: passwordHash,
+        image,
+        phone,
+        address,
+        location,
+        bio,
+        isSeller,        
+    })
+    // if (createUser){
+    //   res.status(201).json({_id: createUser.id, email: createUser.email});
+    // }
+    // else{
+    //   res.status(404);
+    //   throw new Error("User data is not valid");
+    // }
+      res.json({Message: "User Successfully Registered"});
+    }
 };
 
-const login = async (req, res, next) => {
+const loginUsers = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     // Check if email and password exist in the request body
     if (!email || !password) {
-      throw new Error("Invalid email or password");
+      throw new Error("email and password must be provided");
     }
 
     // Assuming loginUser represents the retrieved user data after validation
     const foundUser = await userModel.findOne({ email });
 
-    if (!foundUser || foundUser.password !== password) {
+    if (!foundUser && (await bcrypt.compare(password, foundUser.password !== password))) {
       throw new Error("Invalid email or password");
     }
 
@@ -40,11 +64,11 @@ const login = async (req, res, next) => {
   }
 };
 
-const logout = (req, res) => {
+const logoutUsers = (req, res) => {
   // You can clear the token stored on the client-side upon logout
   res.clearCookie("authToken"); // Clear the JWT token from cookies
 
   res.status(200).json({ message: "User logged out successfully" });
 };
 
-module.exports = { signup, login, logout };
+module.exports = { signupUsers, loginUsers, logoutUsers };
