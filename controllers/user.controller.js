35742cf83c28726500d8d6426006a49bb0331ade
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-31 23:34:29 +0300

    Creating JWT

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,74 +1,108 @@
 const express = require("express");
-const bcrypt = require('bcrypt');
-require("../routes/user.routes");
+const bcrypt = require("bcrypt");
 const userModel = require("../models/user.model");
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
 
 const signupUsers = async (req, res) => {
-  //Destruct the data
-  const { fullName, email, password, image, phone, address, location, bio, isSeller } = req.body;
+  try {
+    const {
+      fullName,
+      email,
+      password,
+      image,
+      phone,
+      address,
+      location,
+      bio,
+      isSeller,
+    } = req.body;
 
-  //Validate the fields whether the user filled all the required fields
-    if ( !email || !password) {
+    if (!email || !password) {
       throw new Error("Email and password must be filled");
     }
 
-    const user_Email = await userModel.findOne({email});
-    if(user_Email) {
-        res.status(400);
-        throw new Error ("User email is already Exists");
-    }
-    else{
-      const passwordHash = await bcrypt.hash(password,13);
-      // console.log("The Hash Password is: " + passwordHash);
-
-      const createUser = await userModel.create({
-        fullName,
-        email,
-        password: passwordHash,
-        image,
-        phone,
-        address,
-        location,
-        bio,
-        isSeller,        
-    })
-    // if (createUser){
-    //   res.status(201).json({_id: createUser.id, email: createUser.email});
-    // }
-    // else{
-    //   res.status(404);
-    //   throw new Error("User data is not valid");
-    // }
-      res.json({Message: "User Successfully Registered"});
-    }
-};
-
-const loginUsers = async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    // Check if email and password exist in the request body
-    if (!email || !password) {
-      throw new Error("email and password must be provided");
+    const user_Email = await userModel.findOne({ email });
+    if (user_Email) {
+      res.status(400);
+      throw new Error("User email already exists");
     }
 
-    // Assuming loginUser represents the retrieved user data after validation
+    const passwordHash = await bcrypt.hash(password, 13);
+    const createUser = await userModel.create({
+      fullName,
+      email,
+      password: passwordHash,
+      image,
+      phone,
+      address,
+      location,
+      bio,
+      isSeller,
+    });
+
     const foundUser = await userModel.findOne({ email });
 
-    if (!foundUser && (await bcrypt.compare(password, foundUser.password !== password))) {
-      throw new Error("Invalid email or password");
+    if (foundUser) {
+      const accessToken = jwt.sign(
+        {
+          foundUser: {
+            email: foundUser.email,
+            id: foundUser.id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+        { expiresIn: "30m" }
+      );
+      res.json({ Message: "User Successfully Registered", accessToken });
+    } else {
+      throw new Error("Failed to create user");
     }
-
-    res.status(200).json({ message: "User logged in", user: foundUser });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-const logoutUsers = (req, res) => {
-  // You can clear the token stored on the client-side upon logout
-  res.clearCookie("authToken"); // Clear the JWT token from cookies
 
+const loginUsers = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error("Email and password must be provided");
+    }
+
+    const foundUser = await userModel.findOne({ email });
+
+    if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
+      const accessToken = jwt.sign(
+        {
+          foundUser: {
+            email: foundUser.email,
+            id: foundUser.id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+        { expiresIn: "30m" }
+      );
+
+      res.status(200).json({ message: "User logged in", accessToken });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Email Address or Password");
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const userStatus = asyncHandler(async (req, res) => {
+  res.json(req.foundUser);
+});
+
+const logoutUsers = (req, res) => {
+  res.clearCookie("authToken");
   res.status(200).json({ message: "User logged out successfully" });
 };
 
-module.exports = { signupUsers, loginUsers, logoutUsers };
+module.exports = { signupUsers, loginUsers, userStatus, logoutUsers };
