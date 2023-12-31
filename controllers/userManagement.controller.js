651a919ec6f22762c6c1f64a8a58dcfa2692a12a
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-31 21:30:18 +0300

    Adding the logics of user update and delete

--- controllers/userManagement.controller.js
+++ controllers/userManagement.controller.js
@@ -23,5 +23,23 @@ const getUser =asyncHandler(async (req, res) => {
     res.status(200).json(find_user_by_id);
     });
 
+const updateUsers = asyncHandler(async (req, res) => {
+        // first get the contact by its id 
+        const find_user_by_id= await user_Model.findById(req.params.id);
+        if (!find_user_by_id) {
+             res.status(404);
+             throw new Error("Contact not found");
+        }
+        // then update the contact
+        const update_user = await user_Model.findByIdAndUpdate(req.params.id, req.body,  {new: true});
+        res.status(200).json(update_user);
+    });
+
+    const deleteUsers =asyncHandler(async (req, res) => {
+        const delete_user= await user_Model.findByIdAndDelete(req.params.id);
+        res.status(200).json(delete_user);
+    });
+    
+
 // Export the getUsers function
-module.exports = { getUsers, getUser };
+module.exports = { getUsers, getUser, updateUsers, deleteUsers };
