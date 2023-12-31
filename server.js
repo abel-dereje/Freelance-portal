e18dd6179439a6011ba5c4dba16c8274bd66eeb6
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-31 03:33:20 +0300

    Adding Middleware on server page

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express=require('express');
-const router = require('./routes/user.routes');
-// const routerConversion = require('./routes/conversion.routes');
-const db_connect = require('./config/db')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const db_connect = require('./config/db');
+const errorHandler = require('./middleware/errorHandling');
+const userRoutes = require('./routes/user.routes');
+const userManagementRoutes = require('./routes/userManagement.routes');
 
 // Calling database function
 db_connect();
 
-// creating express application 
+// Creating express application
 const app = express();
-
-
-// Middleware JSON file for client requests
 app.use(express.json());
-app.use(router);
-// app.use(routerConversion);
 
-// Error handlers Middleware
+// Routes
+app.use(userRoutes);
+app.use(userManagementRoutes);
 
+// Error handling middleware
+// app.use(errorHandler);
 
+// Port number
+const port = process.env.PORT || 4000;
 
-// port number 
-const port=4000;
-
-// i have created the express application then the express app should be listening on port
-app.listen(port,()=>{
-    console.log(`Server listing or running on port ${port}`);
+// Server listening
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
