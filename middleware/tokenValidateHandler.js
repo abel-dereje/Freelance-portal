b73649ae9e427bb056226c38e2cbb86ba7b4cb7b
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2023-12-31 21:42:28 +0300

    Authorization for each router

--- middleware/tokenValidateHandler.js
+++ middleware/tokenValidateHandler.js
@@ -1,2 +1,25 @@
 const asyncHandler = require('express-async-handler');
 const jwt= require('jsonwebtoken');
+
+const tokenHandler= asyncHandler(async(req, res, next)=>{
+    let token;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer")){
+        token = authHeader.split(" ")[1];
+        jwt.verify(token,process.env.ACCESS_TOKE_SECRET_KEY,(err,decoded)=>{
+            if (err){
+                res.status(401);
+                throw new Error ('User not authenticated');
+            }
+            console.log(decoded);
+            req.thisUser=decoded.thisUser;
+            next();
+        });
+        if(!token){
+            res.status(401);
+            throw new Error ('token is required');
+        }
+      }
+    });
+    
+    module.exports =tokenHandler;
