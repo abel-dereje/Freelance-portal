19c3e9c40b42973d6a2b04673b6fe30fbce4f613
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-01-07 23:29:21 +0300

    Creating for routes the user management

--- routes/userManagement.routes.js
+++ routes/userManagement.routes.js
@@ -1,25 +1,21 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 const {
-    getUsers,
-    getUser,
-     updateUsers,
-    deleteUsers
-} = require('../controllers/userManagement.controller');
-const tokenHandler=require('../middleware/tokenValidateHandler');
+  getUsers,
+  createUsers,
+  getUser,
+  updateUsers,
+  deleteUsers,
+} = require("../controllers/userManagement.controller");
+const tokenHandler = require("../middleware/tokenValidateHandler");
 
 // Define middleware
 routes.use(tokenHandler);
 
 // Define routes for managing users
-routes.route("/")
-    .get(getUsers)
-    // .post(createUsers);
+routes.route("/").get(getUsers).post(createUsers);
 
-routes.route("/:id")
-    .get(getUser)
-    .put(updateUsers)
-    .delete(deleteUsers);
+routes.route("/:id").get(getUser).put(updateUsers).delete(deleteUsers);
 
 // Export the routes
 module.exports = routes;
