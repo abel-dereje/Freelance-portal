0b1fa58fa8838aabec1e94e569c7bcfd8fe1e68e
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-01-07 23:25:36 +0300

    Debugging the jwt and adding the payload data

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,7 @@ const userModel = require("../models/user.model");
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 
+
 const signupUsers = async (req, res) => {
   try {
     const {
@@ -24,12 +25,11 @@ const signupUsers = async (req, res) => {
 
     const user_Email = await userModel.findOne({ email });
     if (user_Email) {
-      res.status(400);
-      throw new Error("User email already exists");
+      return res.status(400).json({ error: "User email is already exists" });
     }
 
     const passwordHash = await bcrypt.hash(password, 13);
-    const createUser = await userModel.create({
+    await userModel.create({
       fullName,
       email,
       password: passwordHash,
@@ -41,45 +41,33 @@ const signupUsers = async (req, res) => {
       isSeller,
     });
 
-    const foundUser = await userModel.findOne({ email });
-
-    if (foundUser) {
-      const accessToken = jwt.sign(
-        {
-          foundUser: {
-            email: foundUser.email,
-            id: foundUser.id,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECRET_KEY,
-        { expiresIn: "30m" }
-      );
-      res.json({ Message: "User Successfully Registered", accessToken });
-    } else {
-      throw new Error("Failed to create user");
-    }
+    res.json({ Message: "User Successfully Registered" });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-
-const loginUsers = async (req, res, next) => {
+const loginUsers = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
       throw new Error("Email and password must be provided");
     }
+    let thisUser;
 
-    const foundUser = await userModel.findOne({ email });
+    thisUser = await userModel.findOne({ email });
 
-    if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
+    if (thisUser && (await bcrypt.compare(password, thisUser.password))) {
       const accessToken = jwt.sign(
         {
-          foundUser: {
-            email: foundUser.email,
-            id: foundUser.id,
+          thisUser: {
+            email: thisUser.email,
+            id: thisUser.id,
+            phone: thisUser.phone,
+            address: thisUser.address,
+            location: thisUser.location,
+            bio: thisUser.bio
           },
         },
         process.env.ACCESS_TOKEN_SECRET_KEY,
@@ -88,8 +76,7 @@ const loginUsers = async (req, res, next) => {
 
       res.status(200).json({ message: "User logged in", accessToken });
     } else {
-      res.status(401);
-      throw new Error("Invalid Email Address or Password");
+      res.status(401).json({ error: "Invalid Email Address or Password" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -97,7 +84,7 @@ const loginUsers = async (req, res, next) => {
 };
 
 const userStatus = asyncHandler(async (req, res) => {
-  res.json(req.foundUser);
+  res.json(req.thisUser);
 });
 
 const logoutUsers = (req, res) => {
