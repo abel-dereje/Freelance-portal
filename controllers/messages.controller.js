080924c376b8206ff35e04094d7b7dd8ccbfdd35
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-01-10 22:28:05 +0300

    Creating the delete, update and get by id

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -20,6 +20,7 @@ const createMessage = asyncHandler(async (req, res) => {
       console.log("The Created New Message is:", req.body);
   });
 
+  //Get single message routes by ID
   const getMessage = asyncHandler(async (req, res) => {
     // get contact using by id
     const find_user_by_id = await messages_model.findById(req.params.id);
@@ -30,4 +31,27 @@ const createMessage = asyncHandler(async (req, res) => {
     res.status(200).json(find_user_by_id);
   });
 
-  module.exports = { createMessage, getMessage }
+ //Get update message routes by ID
+const updateMessage = asyncHandler(async (req, res) => {
+    // first get the contact by its id
+    const find_user_by_id = await messages_model.findById(req.params.id);
+    if (!find_user_by_id) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    // then update the skill
+    const update_message = await messages_model.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(update_message);
+  });
+
+  //Get delete skill routes by ID
+const deleteMessage = asyncHandler(async (req, res) => {
+    const delete_message = await messages_model.findByIdAndDelete(req.params.id);
+    res.status(200).json(delete_message);
+  });
+
+  module.exports = { createMessage, getMessage, updateMessage, deleteMessage }
