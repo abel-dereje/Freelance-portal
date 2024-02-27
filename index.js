0c0646c1f6028aab66eac944af29bbbee463a96a
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-02-27 23:09:57 +0300

    Creating the package and re arange the code

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 // creating express 
 const express = require('express');
+
 // create dotenv config
-const dotenv= require('dotenv').config(); 
+const dotenv = require('dotenv').config(); 
 
 // loading or require the routes folder 
 const skillRoutes = require('./routes/skill.routes');
-const userRoutes  = require('./routes/user.routes');
+const userRoutes = require('./routes/user.routes');
 const orderRoutes = require('./routes/order.routes');
-const messageRoutes  = require('./routes/message.routes');
+const messageRoutes = require('./routes/message.routes');
 const conversationRoutes = require('./routes/conversation.routes');
-// const viewRoutes  = require('./routes/view.routes');
 
 // loading or require the middleware custom 
-const errorHandler= require('./middleware/errorHandling');
+const errorHandler = require('./middleware/errorHandling');
 const db_connect = require('./config/db');
 
 // calling the database connection
 db_connect();
 
-//creating the express app
+// creating the express app
 const app = express();
 
 // applying a middleware to access the JSON from the client 
 app.use(express.json());
-//creating a port number
-const port = process.env.PORT || 4000;
 
-//middleware routes
+// middleware routes
 app.use(skillRoutes);
 app.use(userRoutes);
 app.use(orderRoutes);
@@ -34,9 +32,10 @@ app.use(conversationRoutes);
 app.use(messageRoutes);
 
 // we have to use the errorHandler 
-app.use(errorHandler);
+//  app.use(errorHandler);
 
 // creating the express app with the port number to listen on
-app.listen(port,() => {
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- middleware/tokenValidateHandler.js
+++ middleware/tokenValidateHandler.js
@@ -1,24 +1,26 @@
-const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const tokenHandler = asyncHandler(async (req, res, next) => {
   try {
     let token;
-    let authHeader = req.headers.authorization || req.headers.Authorization;
+    const authHeader = req.headers.authorization || req.headers.Authorization;
 
     if (authHeader && authHeader.startsWith('Bearer')) {
       token = authHeader.split(' ')[1];
     }
 
     if (!token) {
-      res.status(401);
-      throw new Error('Token is required');
+      return res.status(401).send({ error: 'Token is required' });
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
       if (err) {
-        res.status(401);
-        throw new Error('User not authenticated or token is invalid');
+        return res.status(401).send({ error: 'User not authenticated or token is invalid' });
       }
       console.log(decoded);
       req.thisUser = decoded.thisUser;
