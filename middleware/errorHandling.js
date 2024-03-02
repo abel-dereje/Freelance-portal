0a14270f819369a17bd9c2a2e796330f9619e5fe
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-03-03 01:08:57 +0300

    Rewrite the error handling

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,31 +1,29 @@
-// using the constant.js
+// errorHandler.js
 const { constants } = require('./constant');
-// we are passing four parameters for middleware 
-const errorHandler = (error,req,res,next) => {
-// we have define the status code 
-const statusCode =res.statusCode ? res.statusCode : 500 ;
-//creating switch case define the status code 
-switch (statusCode) {
-    case constants.VALIDATION_ERROR:
-        res.json({tittle:"Validation Error", stackTrace: error.stack});
-        break ;
-    case constants.UNAUTHORIZED:
-        res.json({tittle:"Unauthorized", stackTrace: error.stack});
-        break ;
-    case constants.FORBIDDEN:
-        res.json({tittle:"Forbidden", stackTrace: error.stack});
-        break ;
-    case constants.NOT_FOUND:
-        res.json({tittle:"Not Found", stackTrace: error.stack});
-        break ;
-    case constants.SERVER_ERROR:
-        res.json({tittle:"Server Error", stackTrace: error.stack});
-        break ;
-    default:
-        console.log("No Error It is Fine");
-        break ;
-}
 
+const errorHandler = (error, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ title: "Validation Error", stackTrace: error.stack });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({ title: "Unauthorized", stackTrace: error.stack });
+            break;
+        case constants.FORBIDDEN:
+            res.json({ title: "Forbidden", stackTrace: error.stack });
+            break;
+        case constants.NOT_FOUND:
+            res.json({ title: "Not Found", stackTrace: error.stack });
+            break;
+        case constants.SERVER_ERROR:
+            res.json({ title: "Server Error", stackTrace: error.stack });
+            break;
+        default:
+            console.log("No Error. Everything is fine.");
+            break;
+    }
 };
 
-module.exports =errorHandler;
+module.exports = errorHandler;
