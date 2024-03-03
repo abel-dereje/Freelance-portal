c176cbeff3637854efa3a3eb7f482922320d19bf
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-03-04 00:03:16 +0300

    changing  res.json to res.status

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,27 +1,27 @@
-// errorHandler.js
 const { constants } = require('./constant');
 
 const errorHandler = (error, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
+    const statusCode = error.status || res.statusCode || constants.SERVER_ERROR;
 
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
-            res.json({ title: "Validation Error", stackTrace: error.stack });
+            res.status(constants.VALIDATION_ERROR).json({ title: "Validation Error", stackTrace: error.stack });
             break;
         case constants.UNAUTHORIZED:
-            res.json({ title: "Unauthorized", stackTrace: error.stack });
+            res.status(constants.UNAUTHORIZED).json({ title: "Unauthorized", stackTrace: error.stack });
             break;
         case constants.FORBIDDEN:
-            res.json({ title: "Forbidden", stackTrace: error.stack });
+            res.status(constants.FORBIDDEN).json({ title: "Forbidden", stackTrace: error.stack });
             break;
         case constants.NOT_FOUND:
-            res.json({ title: "Not Found", stackTrace: error.stack });
+            res.status(constants.NOT_FOUND).json({ title: "Not Found", stackTrace: error.stack });
             break;
         case constants.SERVER_ERROR:
-            res.json({ title: "Server Error", stackTrace: error.stack });
+            res.status(constants.SERVER_ERROR).json({ title: "Server Error", stackTrace: error.stack });
             break;
         default:
-            console.log("No Error. Everything is fine.");
+            console.error("No Error. Everything is fine.");
+            res.status(constants.SERVER_ERROR).json({ title: "Unknown Error", stackTrace: error.stack });
             break;
     }
 };
