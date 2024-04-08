292950f90ae9ee94be47568b866fea9f88fddc80
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-08 18:56:41 +0300

    Adding the route to the main App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-import List from "./pages/list/List";
+import List from "./pages/list/userList/User";
+import Skill from "./pages/list/skill/Skill";
+import Profile from "./pages/list/profile/Profile";
+import Conversation from "./pages/list/conversation/Conversation";	
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -23,13 +26,53 @@ function App() {
             <Route path="signup" element={<Signup />} />
             <Route path="users">
               <Route index element={<List />} />
+              {/* <Route path=":userId" element={<Single />} /> */}
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="profiles">
+              <Route index element={<Profile />} />
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
                 element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
-            <Route path="products">
+            <Route path="skills">
+              <Route index element={<Skill />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="conversations">
+              <Route index element={<Conversation />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+            <Route path="your-network">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+            <Route path="reports">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+            <Route path="apps-and-offers">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
               <Route
