8bfa9445d97bc90526b1aabd849fef215f528632
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-08 18:57:48 +0300

    Changing the data and comments unnecessary files

--- client/src/datatablesource.js
+++ client/src/datatablesource.js
@@ -1,25 +1,25 @@
 
-import axios from 'axios';
- export const userColumns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstname", headerName: "First Name", width: 150 },
-    { field: "lastname", headerName: "Last Name", width: 150 },
-    { field: "email", headerName: "Email", width: 230 },
-    { field: "password", headerName: "Password", width: 150 },
-    { field: "country", headerName: "Country", width: 150 },
-    // Add more columns as needed
-  ];
+// import axios from 'axios';
+//  export const userColumns = [
+//     { field: "id", headerName: "ID", width: 70 },
+//     { field: "firstname", headerName: "First Name", width: 150 },
+//     { field: "lastname", headerName: "Last Name", width: 150 },
+//     { field: "email", headerName: "Email", width: 230 },
+//     { field: "password", headerName: "Password", width: 150 },
+//     { field: "country", headerName: "Country", width: 150 },
+//     // Add more columns as needed
+//   ];
   
   
  
 
-// // Define the userRows constant
+// // // Define the userRows constant
 // export const userRows = [];
 
 // // Fetch data from MongoDB using API
 // const fetchDataFromMongoDB = async () => {
 //   try {
-//     const response = await axios.get('http://localhost:4000/users/:id'); 
+//     const response = await axios.get('http://localhost:4000/users'); 
 //     return response.data; // Assuming the response contains an array of user objects
 //   } catch (error) {
 //     console.error('Error fetching user data:', error);
