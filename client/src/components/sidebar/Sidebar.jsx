c55223519383689b84f660767c49bba81f10a054
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-14 13:11:46 +0300

    commenting  the unused material icons

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import StoreIcon from "@mui/icons-material/Store";
+// import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+// import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+// import CreditCardIcon from "@mui/icons-material/CreditCard";
+// import StoreIcon from "@mui/icons-material/Store";
+import GroupIcon from '@mui/icons-material/Group';
 import InsertChartIcon from "@mui/icons-material/InsertChart";
 import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
@@ -11,6 +12,17 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import AppsIcon from '@mui/icons-material/Apps';
+import SchoolIcon from '@mui/icons-material/School';
+import ChatIcon from '@mui/icons-material/Chat';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+
+
+
+
+
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
@@ -37,24 +49,58 @@ const Sidebar = () => {
           <p className="title">LISTS</p>
           <Link to="/users" style={{ textDecoration: "none" }}>
             <li>
-              <PersonOutlineIcon className="icon" />
+              <GroupIcon className="icon" />
               <span>Users</span>
             </li>
           </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
+          <Link to="/profiles" style={{ textDecoration: "none" }}>
             <li>
-              <StoreIcon className="icon" />
-              <span>Products</span>
+              <PhotoLibraryIcon className="icon" />
+              <span>Profiles</span>
             </li>
           </Link>
+          <Link to="/skills" style={{ textDecoration: "none" }}>
+            <li>
+              <SchoolIcon className="icon" />
+              <span>Skills</span>
+            </li>
+          </Link>
+          <Link to="/conversations" style={{ textDecoration: "none" }}>
+            <li>
+              <ChatIcon className="icon" />
+              <span>Conversations</span>
+            </li>
+          </Link>
+          <Link to="/your-network" style={{ textDecoration: "none" }}>
           <li>
-            <CreditCardIcon className="icon" />
-            <span>Orders</span>
+            <PeopleAltIcon className="icon" />
+            <span>Your Network</span>
           </li>
+          </Link>
+          <Link to="/reports" style={{ textDecoration: "none" }}>
+          <li>
+            <MonetizationOnIcon className="icon" />
+            <span>Reports</span>
+          </li>
+          </Link>
+          {/* <Link to="/my-requests" style={{ textDecoration: "none" }}>
           <li>
             <LocalShippingIcon className="icon" />
-            <span>Delivery</span>
+            <span>My Requests</span>
           </li>
+          </Link> */}
+          <Link to="/apps-and-offers" style={{ textDecoration: "none" }}>
+          <li>
+            <AppsIcon className="icon" />
+            <span>Apps and Offers</span>
+          </li>
+          </Link>
+          {/* <Link to="/reports" style={{ textDecoration: "none" }}>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Reports</span>
+          </li>
+          </Link> */}
           <p className="title">USEFUL</p>
           <li>
             <InsertChartIcon className="icon" />
