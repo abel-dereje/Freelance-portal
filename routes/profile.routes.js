53ad6ea313a104a4ccd421c28f168ec513156f4d
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-14 13:19:04 +0300

    Creating routes for all profiles

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const routes = express.Router();
+const cors = require('cors');
 const { getProfiles, createProfile, getProfile, updateProfile, deleteProfile } = require("../controllers/profile.controller");
 
 const profileRouter = express.Router();
 
+// Configure CORS
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+// Enable CORS for all routes
+profileRouter.use(cors(corsOptions));
 
 // Define routes for managing profile
 profileRouter.post('/createProfile', createProfile);
@@ -11,9 +19,6 @@ profileRouter.get('/getProfiles', getProfiles);
 profileRouter.get('/getProfile/:id', getProfile);
 profileRouter.put('/updateProfile/:id', updateProfile);
 profileRouter.delete('/deleteProfile/:id', deleteProfile);
-            
-
-routes.route("/profile/:id").get(getProfile).put(updateProfile).delete(deleteProfile);
 
 // Export the routes
 module.exports = profileRouter;
