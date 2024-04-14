39c60fd0483f9c120f4bd6cb6e6221afc4b9c925
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-14 13:18:45 +0300

    Creating routes for all conversation

--- routes/conversation.routes.js
+++ routes/conversation.routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const tokenHandler = require('../middleware/tokenValidateHandler');
-const { createConversation, getConversation, updateConversation, deleteConversation } = require('../controllers/conversations.controller.js');
+const { createConversation, getConversations, getConversation, updateConversation, deleteConversation } = require('../controllers/conversations.controller.js');
 
 const routerConversation = express.Router();
 
 // Use middleware for all routes on this router
-routerConversation.use(tokenHandler);
+//routerConversation.use(tokenHandler);
+routerConversation.use(cors());
 
 
 // Define routes
 routerConversation.route("/conversations").post(createConversation);
+routerConversation.route("/getConversations").get(getConversations);
 
 routerConversation.route("/conversations/:id").get(getConversation).put(updateConversation).delete(deleteConversation);
 
