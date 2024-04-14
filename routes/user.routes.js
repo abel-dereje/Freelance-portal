7087ff911c2d580bd458622e74e3a49f6452be62
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-04-14 13:19:26 +0300

    Creating routes for all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signupUsers, loginUsers, userStatus, logoutUsers } = require('../controllers/user.controller');
+const cors = require('cors')
+const { signupUsers, loginUsers, getUsers, userStatus, logoutUsers } = require('../controllers/user.controller');
 const tokenHandler = require('../middleware/tokenValidateHandler');
 
 const router = express.Router();
 //router.use(tokenHandler); // Use the token handler middleware for all profile routes
 
+router.use(cors());
 
 // Route for user signup (without token middleware)
 router.post('/signup', signupUsers);
@@ -13,7 +15,10 @@ router.post('/signup', signupUsers);
 router.post('/login', loginUsers);
 
 // Route for user status
-router.get('/status', tokenHandler, userStatus);
+ router.get('/status', tokenHandler, userStatus);
+
+// Route for getting all users
+router.get('/users', getUsers);
 
 // Route for user logout
 router.post('/logout', tokenHandler, logoutUsers);
